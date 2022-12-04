20409f76c0046d2a180711517cd95498f975128b
Hikariyen881230 <hikariyen881230@gmail.com>
2022-12-04 16:00:40 +0800

    change

--- JS/callback1.js
+++ JS/callback1.js
@@ -1,4 +1,4 @@
-let doWork = function (job, job2, job3, timer, cb) {
+let doWork = function (job, timer, job2, timer2, job3, timer3, cb) {
 
     setTimeout(() => {
         let now = new Date();
@@ -6,13 +6,13 @@ let doWork = function (job, job2, job3, timer, cb) {
 
         setTimeout(() => {
             let now = new Date();
-            cb(null, `完成工作 ${job3} at ${now.toISOString()}`);
+            cb(null, `完成工作 ${job2} at ${now.toISOString()}`);
 
             setTimeout(() => {
                 let now = new Date();
                 cb(null, `完成工作 ${job3} at ${now.toISOString()}`);
-            }, 3000);
-        }, 5000);
+            }, timer3);
+        }, timer2);
 
     }, timer);
 
@@ -21,6 +21,6 @@ let doWork = function (job, job2, job3, timer, cb) {
 let now = new Date();
 console.log(`工作開始 at ${now.toISOString()}`);
 // 刷牙 3 秒鐘 -> 吃早餐 5 秒鐘 -> 寫功課 3 秒鐘
-doWork('刷牙', '吃早餐', '寫功課', 3000, (err, data) => {
+doWork('刷牙', 3000, '吃早餐', 5000, '寫功課', 3000, (err, data) => {
     console.log(data);
 });
