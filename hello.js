78e964db8048b9c0900143546228cbde551f4907
Hikariyen881230 <hikariyen881230@gmail.com>
2022-12-04 15:54:45 +0800

    add first work

--- hello.js
+++ hello.js
@@ -1,1 +1,13 @@
-console.log("hello world!")
+console.log("hello world!");
+
+let person = {
+    name: "aimer",
+    age: 25,
+    city: "taipei",
+};
+
+let another = { ...person, age: 29 };
+console.log(another.age);
+for (let i = 0; i <= 10; i++) {
+    console.log(i);
+}
