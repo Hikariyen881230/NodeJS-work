02c50d6f110e55f90c36462ea47dbd537def7f2c
Hikariyen881230 <hikariyen881230@gmail.com>
2022-12-10 12:22:51 +0800

    modified

--- JS/readfile-promise.js
+++ JS/readfile-promise.js
@@ -4,9 +4,9 @@ let p = new Promise((resolve, reject) => {
     setTimeout(() => {
         fs.readFile("text.txt", "utf-8", (err, data) => {
             if (err) {
-                reject('讀取檔案失敗', err);
+                reject(err);
             } else {
-                resolve('成功讀取檔案', data);
+                resolve(data);
             }
         });
     }, 3000);
